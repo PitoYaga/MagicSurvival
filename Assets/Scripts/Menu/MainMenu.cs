7a55dce5e8829be01cb174d41327ca6c197b3120
alpcan818 <124262462+alpcan818@users.noreply.github.com>
2023-02-03 13:01:14 +0300

    Scene Manager CS
    credits scene

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelScene()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+}
